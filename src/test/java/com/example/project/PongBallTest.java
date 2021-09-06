1692929fa6d475ce7d003cc8e865cfdb7a2b3f30
skalakm <skalakm@dickinson.edu>
2021-09-06 17:21:42 -0400

    Add files via upload

---
+++ src/test/java/com/example/project/PongBallTest.java
@@ -0,0 +1,14 @@
+package pong;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.*;
+
+public class PongBallTest {
+
+   @Test
+   public void testHelloWorld()
+   {
+      
+      assertEquals(5,6);
+   }
+}
